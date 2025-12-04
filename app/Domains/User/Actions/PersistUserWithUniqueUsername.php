12bbf2c6097ed2edb1c992caeb6ad6f935e2c5b9
Danny Foster <danny@northwestern.edu>
2025-12-03 18:12:42 -0600

    feat: default role assignment

--- app/Domains/User/Actions/PersistUserWithUniqueUsername.php
+++ app/Domains/User/Actions/PersistUserWithUniqueUsername.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Domains\User\Actions;
 
 use App\Domains\User\Actions\Directory\FindOrUpdateUserFromDirectory;
+use App\Domains\User\Enums\AuthTypeEnum;
+use App\Domains\User\Enums\SystemRoleEnum;
 use App\Domains\User\Models\User;
 use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Support\Facades\DB;
@@ -12,6 +14,8 @@ use Illuminate\Support\Facades\DB;
 /**
  * Persists a user with a unique username by handling potential uniqueness constraint violations
  * and retrieving the existing user if a conflict occurs.
+ *
+ * For SSO users, the default "Northwestern User" system role is assigned.
  *
  * {@see FindOrUpdateUserFromDirectory}
  */
@@ -22,6 +26,10 @@ final class PersistUserWithUniqueUsername
         return DB::transaction(function () use ($user) {
             try {
                 $user->save();
+
+                if ($user->auth_type === AuthTypeEnum::SSO) {
+                    $user->assignRole(SystemRoleEnum::NORTHWESTERN_USER);
+                }
 
                 return $user;
             } catch (UniqueConstraintViolationException) {
